2a440644dfb586fac7c46b0f568c65a9ca3e8dbf
Alibek Manabayev <alibek.manabayev@gmail.com>
2026-04-21 22:32:26 +0100

    WORKING: text in chat

--- src/main/kotlin/kz/minmax/mbdebugger/items/DebuggerItem.kt
+++ src/main/kotlin/kz/minmax/mbdebugger/items/DebuggerItem.kt
@@ -2,6 +2,7 @@ package kz.minmax.mbdebugger.items
 
 import com.gregtechceu.gtceu.api.machine.MetaMachine
 import com.gregtechceu.gtceu.api.machine.multiblock.MultiblockControllerMachine
+import com.gregtechceu.gtceu.api.pattern.error.PatternError
 import net.minecraft.network.chat.Component
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.item.Item
@@ -27,16 +28,34 @@ class DebuggerItem : Item(Properties()) {
     val error = state.error
 
     if (error != null) {
-      LOGGER.info("Multiblock error: ${error.errorInfo.getString(120)}")
-      player.displayClientMessage(
-              Component.literal("Unknown Error:").append(error.errorInfo),
-              false
-      )
+      val errorMessage = formatError(error)
+
+      LOGGER.info("Multiblock error: ${errorMessage.getString(120)}")
+      player.displayClientMessage(errorMessage, false)
     } else {
       LOGGER.info("Multiblock is valid")
       player.displayClientMessage(Component.literal("Multiblock is valid!"), false)
     }
     return InteractionResult.sidedSuccess(level.isClientSide)
+  }
+
+  private fun formatError(error: PatternError): Component {
+    // Check if error is an instance of base class PatternError
+    if (error::class == PatternError::class) {
+      val component = Component.literal("Expected components: ")
+      val candidates = error.candidates
+      var first = true
+      for (candidateList in candidates) {
+        if (candidateList.isNotEmpty()) {
+          if (!first) component.append(Component.literal(", "))
+          component.append(candidateList[0].displayName)
+          first = false
+        }
+      }
+      component.append(Component.literal(" at (${error.pos.x}, ${error.pos.y}, ${error.pos.z})"))
+      return component
+    }
+    return Component.literal("Unknown Error: ").append(error.errorInfo)
   }
 
   companion object {
